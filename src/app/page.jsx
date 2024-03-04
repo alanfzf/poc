6d296e2dfa12de068bc0a4e0353e39565ff498d5
alanfzf <alandavidg16@gmail.com>
2024-03-04 14:13:48 -0600

    Updates

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,38 @@
-import { Input } from "@nextui-org/react";
-import { Button } from "@nextui-org/button";
-import { Link } from "@nextui-org/react";
-import Image from "next/image";
+"use client"
+import { Input } from "@nextui-org/react"
+import { Button } from "@nextui-org/button"
+import { Link } from "@nextui-org/react"
+import Image from "next/image"
+
+import { useRouter } from "next/navigation"
+import { useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
+
+const schema = yup.object().shape({
+  correo: yup
+    .string()
+    .email("El formato de correo es invalido")
+    .required("Este es un campo requerido"),
+  clave: yup.string().required("Este es un campo requerido"),
+})
 
 export default function Home() {
+  const router = useRouter()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    mode: "all",
+    resolver: yupResolver(schema),
+  })
+
+  const onSubmit = async (_data) => {
+    await new Promise((resolve) => setTimeout(resolve, 800))
+    router.push("/404", { scroll: false })
+  }
+
   return (
     <main className="absolute inset-0 -z-10 h-screen w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#BEF264_100%)]">
       <div className="grid grid-cols-1 lg:grid-cols-2 h-full">
@@ -16,42 +45,49 @@ export default function Home() {
           />
         </div>
         <div className="flex items-center justify-center">
-          {/* sm: 640px */}
-          {/* md: 768px */}
-          {/* lg: 1024px */}
-          {/* xl: 1280px */}
-          {/* 2xl: 1536px */}
-
           <div className="w-full  md:w-2/3 lg:w-10/12 xl:w-7/12">
             <div className="mb-3">
               <p className="text-6xl font-bold">Bienvenido</p>
             </div>
-
-            <Input
-              type="email"
-              label="Correo electronico"
-              isRequired
-              isClearable
-              variant="bordered"
-              className="mb-3"
-            />
-            <Input
-              type="password"
-              label="Contraseña"
-              isRequired
-              isClearable
-              variant="bordered"
-              className="mb-3"
-            />
-            <div className="mb-3">
-              <Link className="text-white">¿Olvidaste tu contraseña?</Link>
-            </div>
-            <Button color="primary" size="lg">
-              Iniciar sesión
-            </Button>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <Input
+                {...register("correo")}
+                type="email"
+                label="Correo electronico"
+                isRequired
+                isClearable
+                variant="bordered"
+                className="mb-3"
+                isInvalid={errors.correo}
+                errorMessage={errors.correo?.message}
+              />
+              <Input
+                {...register("clave")}
+                type="password"
+                label="Contraseña"
+                isRequired
+                isClearable
+                variant="bordered"
+                className="mb-3"
+                isInvalid={errors.clave}
+                errorMessage={errors.clave?.message}
+              />
+              <div className="mb-3">
+                <Link className="text-white" href="/recoverPassword">
+                  ¿Olvidaste tu contraseña?
+                </Link>
+              </div>
+              <Button
+                color="primary"
+                size="lg"
+                type="submit"
+                isLoading={isSubmitting}>
+                Iniciar sesión
+              </Button>
+            </form>
           </div>
         </div>
       </div>
     </main>
-  );
+  )
 }
---
+++ src/app/recoverPassword/page.jsx
@@ -0,0 +1,74 @@
+"use client"
+import { Input } from "@nextui-org/react"
+import { Button } from "@nextui-org/button"
+import { useRouter } from "next/navigation"
+
+import { useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
+
+const schema = yup.object().shape({
+  correo: yup.string().email().required(),
+})
+
+function RestorePassword() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    mode: "all",
+    resolver: yupResolver(schema),
+  })
+
+  const router = useRouter()
+
+  const goBack = () => router.push("/", { scroll: false })
+
+  const sendData = async (data) => {
+    console.log(data)
+    await new Promise((resolve) => setTimeout(resolve, 1500))
+    alert("Solicitud enviada con exito!")
+    goBack()
+  }
+
+  return (
+    <main className="h-screen">
+      <div className="h-full flex items-center justify-center">
+        <div className="w-full md:w-2/3 lg:w-2/3 xl:w-1/3 rounded rounded-lg border border-gray-500 p-5">
+          <div className="mb-4">
+            <p className="text-3xl font-bold">Recuperar contraseña</p>
+          </div>
+
+          <form onSubmit={handleSubmit(sendData)}>
+            <Input
+              {...register("correo")}
+              type="email"
+              label="Correo electronico"
+              isRequired
+              isClearable
+              variant="bordered"
+              className="mb-4"
+              isInvalid={!!errors.correo}
+              errorMessage={errors.correo?.message}
+            />
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+              <Button
+                type="submit"
+                color="primary"
+                size="lg"
+                isLoading={isSubmitting}>
+                Realizar solicitud
+              </Button>
+              <Button color="secondary" size="lg">
+                Volver
+              </Button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default RestorePassword
