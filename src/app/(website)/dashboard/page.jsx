96515db3c4270c97075631d41bcef6287da734fa
alanfzf <alandavidg16@gmail.com>
2024-03-04 22:44:06 -0600

    dsakdsa

--- src/app/(website)/dashboard/page.jsx
+++ src/app/(website)/dashboard/page.jsx
@@ -2,9 +2,9 @@ import { Progress } from "@nextui-org/react"
 
 function Dashboard() {
   return (
-    <div class="mx-auto px-4 max-w-screen-lg">
+    <div className="mx-auto px-4 max-w-screen-lg">
       <h1 className="text-3xl font-bold">Dashboard</h1>
-      <div class="flex justify-center items-center h-96">
+      <div className="flex justify-center items-center h-96">
         <Progress
           size="sm"
           isIndeterminate
